f457c57e56c46d0fd41debb9c72bf4bcf3f45b9b
Satya <satyapradhan1732@gmail.com>
2026-01-16 02:10:43 +0530

    working on ethereum AA

---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+
+contract MinimalAccount is IAccount, Ownable {
+
+    /*//////////////////////////////////////////////////////////////
+                            EXTERNAL ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error MINIMALACCOUNT__NotFromEntryPoint();
+    error MINIMALACCOUNT__NotFromEntryPointOrOwner();
+
+
+    /*//////////////////////////////////////////////////////////////
+                        EXTERNAL STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    IEntryPoint private immutable I_ENTRYPOINT;
+
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL MODIFIERS
+    //////////////////////////////////////////////////////////////*/
+
+    modifier requireFromEntryPoint() {
+        if(msg.sender != address(I_ENTRYPOINT)) {
+            revert MINIMALACCOUNT__NotFromEntryPoint();
+        }
+        _;
+    }
+    modifier requireEntryPointOrOwner() {
+        if (msg.sender != address(I_ENTRYPOINT) && msg.sender != owner()) {
+            revert MINIMALACCOUNT__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+    // entrypoint -> this contract
+    /*
+    struct PackedUserOperation {
+        address sender; ///// our minimal account
+        uint256 nonce;  ///// number only used once, nonce
+        bytes initCode; //// ignore for now
+        bytes callData; //// this is where we put "the good stuff". 
+        bytes32 accountGasLimits;
+        uint256 preVerificationGas;
+        bytes32 gasFees;
+        bytes paymasterAndData;
+        bytes signature;
+    }
+    */
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        I_ENTRYPOINT = IEntryPoint(entryPoint);
+    }
+
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    
+   // A signature is valid, if it's the Minimal account owner
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external requireFromEntryPoint returns (uint256 validationData) {
+        // we need to validate signature `bytes signature` against all other parameter in the struct
+        validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce()
+
+        _payPrefund(missingAccountFunds);
+    }
+
+    // userOpHash 
+    // EIP-191 version of the signed hash
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash) internal view returns (uint256 validationData) {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (success);
+        }
+    }
+
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function execute(address dest, uint256 value, bytes calldata functionData) external requireEntryPointOrOwner {}
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    function getEntryPoint() external view returns (address) {
+        return address(I_ENTRYPOINT);
+    }
+}
